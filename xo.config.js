0dc064eb6952695ec8876ef1754dbca9884116fe
Omar Diab <me@omardiab.com>
2020-05-24 00:48:55 +0900

    pathgroups config

--- xo.config.js
+++ xo.config.js
@@ -4,6 +4,20 @@ module.exports = {
 		// If add rules, include a comment explaining why
 
 		// Tons of violations from third party libraries make this error too much
-		'@typescript-eslint/prefer-readonly-parameter-types': 'off'
+		'@typescript-eslint/prefer-readonly-parameter-types': 'off',
+		// Ordering imports makes reading them easier
+		'import/order': [
+			'error',
+			{
+				'newlines-between': 'always',
+				alphabetize: {order: 'asc'}, // Same directories clump together
+				// In tests, source code clumps together
+				pathGroups: [
+					{pattern: '@src/**', group: 'external', position: 'after'},
+					{pattern: '@src', group: 'external', position: 'after'}
+				],
+				pathGroupsExcludedImportTypes: ['builtin']
+			}
+		]
 	}
 };
